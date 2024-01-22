fe3186caf53c8dbae6a94afe4cba9b52550c1375
Foysalrabby <81823837+Foysalrabby@users.noreply.github.com>
2024-01-22 13:19:20 +0600

    TEXTBUTTON

--- classpartice/lib/main.dart
+++ classpartice/lib/main.dart
@@ -13,7 +13,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -25,8 +24,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  
-
   final String title;
 
   @override
@@ -36,35 +33,19 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
-  
-
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
       appBar: AppBar(
-        
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-     
         title: Text(widget.title),
       ),
-      body:Center(
-        child:Container(
-        width: 100,
-        height: 100,
-        color: Colors.red,
-        child: Center(
-         child: Text("Hello",style: TextStyle(
-          color: Colors.white,
-          fontSize: 30,
-          fontWeight:FontWeight.bold,
-          backgroundColor: Colors.black45
-         ),)),
-      ), 
-      ) ,
-      
-      
-      
+      body: TextButton(
+        child: Text("click me"),
+        onPressed: () {
+          print("txt button");
+        },
+      ),
     );
   }
 }
