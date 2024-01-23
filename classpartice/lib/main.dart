adc6374794d42bc796a343545a015ff923ea64c4
Foysalrabby <81823837+Foysalrabby@users.noreply.github.com>
2024-01-23 13:54:21 +0600

    add image change pubspec.yaml and create asset/image
     assets:
        - assets/images/ (folder name)

--- classpartice/lib/main.dart
+++ classpartice/lib/main.dart
@@ -36,15 +36,14 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text(widget.title),
-        ),
-        body: ElevatedButton(
-          child: Text("click here"),
-          onPressed: () {
-            print("object");
-          },
-        ));
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(child: Container(
+        width: 200,
+        height: 200,
+        child: Image.asset("assets/images/bascket.png"))),
+    );
   }
 }
